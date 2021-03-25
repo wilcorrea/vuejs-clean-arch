c4103c85f2428f368a154635c49cf7c84de5e1e1
William Correa <wilcorrea@gmail.com>
2021-03-25 16:09:46 -0300

    1. just some "firulas"

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ message }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -41,11 +41,11 @@ import { Options, Vue } from 'vue-class-component'
 
 @Options({
   props: {
-    msg: String
+    message: String
   }
 })
 export default class HelloWorld extends Vue {
-  msg!: string
+  message!: string
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld message="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
